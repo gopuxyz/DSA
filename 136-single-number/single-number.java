25d0c696a4c90b906846d8ba9d4ed3dd629de7f2
Gopal prashad <158044024+gopuxyz@users.noreply.github.com>
2025-03-05 22:51:42 +0530

    Time: 1 ms (99.85%) | Memory: 46.2 MB (46.24%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+       int  res = 0;
+        for(int ele : nums){
+            res = res^ele;
+        }
+        return res;
+        
+    }
+}
