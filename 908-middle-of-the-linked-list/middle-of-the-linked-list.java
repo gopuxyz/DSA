f39e2c4ea3f0322d064c20b734659895deb1f3d8
Gopal prashad <158044024+gopuxyz@users.noreply.github.com>
2025-04-11 10:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (22.08%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -1,18 +1,14 @@
 
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode temp = head;
-        int len = 0;
-        while(temp!=null){
-            temp = temp.next;
-            len++;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        int mid = len/2 + 1;
-        temp = head;
-        for(int i = 1; i<=mid-1; i++){
-            temp = temp.next;
-        }
-        return temp;
+        return slow;
+
         
     }
 }
