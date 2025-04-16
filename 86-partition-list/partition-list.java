103136773c2a9caade5f29b0540e578c062cf6fa
Gopal prashad <158044024+gopuxyz@users.noreply.github.com>
2025-04-16 23:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.98%) - LeetSync

---
+++ 86-partition-list/partition-list.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode a = new ListNode(-1);
+        ListNode b = new ListNode(-1);
+        ListNode tempA = a;
+        ListNode tempB = b;
+        ListNode temp = head;
+        while(temp!=null){
+            if(temp.val<x){
+              tempA.next = temp;
+              temp = temp.next; // aage bhada do iska means
+              tempA = tempA.next;
+
+            } 
+            else{ // temp.val >=x
+                tempB.next = temp;
+                 temp = temp.next;
+                 tempB = tempB.next;
+            }
+        }
+        tempB.next = null; // last node
+        a = a.next;
+        b = b.next;
+        if(a==null) return b;
+        tempA.next = b; // list  A attached to B
+        return a;
+         
+        
+    }
+}   
