2a5dca496283846338c60207aa3a1731f5801f4f
Gopal prashad <158044024+gopuxyz@users.noreply.github.com>
2025-08-08 17:35:37 +0530

    Time: 1 ms (99.52%) | Memory: 56.7 MB (94.75%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,9 +3,7 @@ class Solution {
         int n = nums.length;
         int maxsum = nums[0];
         int currentsum = 0;
-        if(n==1){
-            return nums[0];
-        }
+        
         for(int i = 0; i<n; i++){
         
             currentsum += nums[i];
