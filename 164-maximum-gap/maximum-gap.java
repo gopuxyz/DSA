8eabd9d778909212adc3a627639161ed9c422764
Gopal prashad <158044024+gopuxyz@users.noreply.github.com>
2025-08-23 23:27:38 +0530

    Time: 239 ms (5.03%) | Memory: 59.6 MB (85.45%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length < 2) return 0;
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int num : nums) {
+            maxHeap.offer(num);
+        }
+
+        int maxDiff = 0;
+        while(!maxHeap.isEmpty()) {
+            int top = maxHeap.poll();
+            if(maxHeap.isEmpty()) break;
+            int next = maxHeap.peek();
+            maxDiff = Math.max(maxDiff, top - next);
+        }
+
+        return maxDiff;
+    }
+}
